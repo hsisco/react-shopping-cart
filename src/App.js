540f9ac922e8b7e1947ee8efe89e325eace426e0
hsisco <hysensisco@gmail.com>
2019-11-04 21:27:53 -0800

    create CartContext, apply to Products and refactor App

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 // Contexts
-import { ProductContext } from "./contexts/ProductContext";
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext } from './contexts/CartContext';
 
 function App() {
 	const [products] = useState(data);
@@ -21,10 +22,12 @@ function App() {
 	return (
 		<div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
+        <CartContext.Provider value={cart}>
+          <Navigation />
 
-        <Route exact path="/" component={Products} />
-        <Route path="/cart" component={ShoppingCart} />
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
 		</div>
 	);
