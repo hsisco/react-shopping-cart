132564ae5e44b9ec1f31903623f01d65658dc76b
hsisco <hysensisco@gmail.com>
2020-01-29 20:55:46 -0800

    Update ShoppingCartItem.js

--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -4,11 +4,9 @@ const Item = props => {
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
-
-
 			<div>
 				<h1>{props.title}</h1>
-				<p>$ {props.price}</p>
+				<p>${props.price}</p>
 				<button>Remove from cart</button>
 			</div>
 		</div>
